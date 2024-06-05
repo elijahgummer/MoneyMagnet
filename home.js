ea6f24f6c5c1b14cd83e0b830aac809ca9a598da
Elijah W Gummer <96103526+elijahgummer@users.noreply.github.com>
2024-06-05 10:02:44 +1000

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-
-
-
-var typed = new Typed(".auto-type", {
-    strings: ["Take Control", "Achieve Financial Freedom", "Master Your Money", "Secure Your Financial Future", "Budget Like a Pro"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-})
