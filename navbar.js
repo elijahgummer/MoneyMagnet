6e6a57edd069d9d612ca9327cdb9342545496a68
Elijah W Gummer <96103526+elijahgummer@users.noreply.github.com>
2024-06-05 10:01:40 +1000

    Delete navbar.js

--- navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const icon = document.querySelector(".icon");
-    const navbarList = document.querySelector(".navbar-list");
-
-    icon.addEventListener("click", function () {
-        navbarList.classList.toggle("show");
-    });
-});
